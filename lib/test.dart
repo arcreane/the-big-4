b02aa87cc5c2e5c00b69b64cc0902563d99709de
sophie009 <61512849+sophie009@users.noreply.github.com>
2021-11-28 00:08:10 +0100

    test de son

---
+++ lib/test.dart
@@ -0,0 +1,31 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AudioPlayerSong extends StatefulWidget {
+  @override
+  _AudioPlayerSong createState() => _AudioPlayerSong();
+}//fin class player
+
+class _AudioPlayerSong extends State<AudioPlayerSong> {
+  AudioPlayer audioPlayer = AudioPlayer();
+  AudioPlayerState audioPlayerState = AudioPlayerState.PAUSED;
+  AudioCache audioCache;
+  String path = 'toc.wav';
+
+  @override
+  void initState(){
+    super.initState();
+    audioCache = AudioCache(fixedPlayer: audioPlayer);
+    audioPlayer.onPlayerStateChanged.listen((event) {
+      
+
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
